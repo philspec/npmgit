db0c17c9737a6c4b0176b6b37094436b1493b0fb
phillips <qwertysonata@gmail.com>
2024-10-15 16:00:19 +0530

    ok

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+// server for fetching the data from npm server an dsending to client
+
+
 const express = require('express');
 const cors = require('cors');
-const app = express();
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 app.get('/:searchTerm/:size', async (req, res) => {
   try {
     const { searchTerm, size = 10 } = req.params;
