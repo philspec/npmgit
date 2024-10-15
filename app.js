4f75f65fd9710cc6e35dbf141fe8d47fedd5428a
phillips <qwertysonata@gmail.com>
2024-10-15 12:53:53 +0530

    ok

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const cors = require('cors');
-const fetch = require('node-fetch');
 
 const app = express();
-app.use(cors());
-
-app.get('/api/package-info', async (req, res) => {
+app.use();
+app.get('/:searchTerm/:size/:ranking', async (req, res) => {
   try {
-    const { searchTerm, size = 10 } = req.query;
+    const { searchTerm, size = 10 , ranking = 'popularity' } = req.params;
     
     // Fetch NPM package data
     const npmResponse = await fetch(`http://registry.npmjs.com/-/v1/search?text=${searchTerm}&size=${size}`);
