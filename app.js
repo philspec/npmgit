a2996d24ddf92ba4842a59b0e5d6867b874aa313
phillips <qwertysonata@gmail.com>
2024-10-15 13:33:37 +0530

    ok

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/:searchTerm/:size/:ranking', async (req, res) => {
     const { searchTerm, size = 10 , ranking = 'popularity' } = req.params;
     
     // Fetch NPM package data
-    const npmResponse = await fetch(`http://registry.npmjs.com/-/v1/search?text=${searchTerm}&size=${size}`);
+    const npmResponse = await fetch(`http://registry.npmjs.com/-/v1/search?text=${searchTerm}&size=${size}&ranking=${ranking}`);
     if (!npmResponse.ok) throw new Error('NPM API request failed');
     const npmData = await npmResponse.json();
     const npmPackages = npmData.objects;
